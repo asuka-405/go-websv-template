430852e684b3424938231de6ac5571752060ae40
suryansh kapil <suryansh2048+git@gmail.com>
2025-02-04 19:17:18 +0530

    setting up db

--- install.sh
+++ install.sh
@@ -1,2 +1,14 @@
-go install github.com/air-verse/air@latest
-// air init
+
+function install_air() {
+  go install github.com/air-verse/air@latest
+  // air init
+}
+
+function install_docker_archlinux() {
+  sudo pacman -S docker
+  sudo systemctl enable docker
+  sudo systemctl start docker
+  sudo usermod -aG docker $USER
+  sudo echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+  sudo sysctl --system
+}
